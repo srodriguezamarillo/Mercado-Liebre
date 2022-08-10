0e2f4580b66364bc25e0c192f1bde3a33835079c
srodriguezamarillo <srodriguezamarillo@gmail.com>
2022-08-10 20:10:31 -0300

    Ej02 terminado

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use(express.static('public'))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/views/home.html')))
+app.listen(3000, () => console.log('Listening on 3000....'))
